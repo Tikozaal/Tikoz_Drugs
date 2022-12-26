e2f9b7adef9b98fc59419f331eab6f452b98ea95
Tikoz <106456046+Tikozaal@users.noreply.github.com>
2022-12-27 00:25:22 +0100

    Add files via upload

---
+++ Tikoz_Drugs/sql.sql
@@ -0,0 +1,10 @@
+INSERT INTO `items` (name, label, weight) VALUES
+('weed', "weed", 0.2),
+('weed_pooch', "pochon de weed", 0.2),
+('coke', "coke", 0.2),
+('coke_pooch', "pochon de coke", 0.2),
+('meth', "meth", 0.2),
+('meth_pooch', "pochon de meth", 0.2),
+('opium', "opium", 0.2),
+('opium_pooch', "pochon d'opium", 0.2)
+;
